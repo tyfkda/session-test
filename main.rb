03f14a31fd17765c32de121921d88adb263534b5
tyfkda <tyfkda@gmail.com>
2015-02-14 13:40:15 +0900

    Read cookie value and returns incremented value

--- main.rb
+++ main.rb
@@ -1,19 +1,34 @@
+require 'erb'
 require './my_http_server'
 
 if $0 == __FILE__
   http_server = MyHttpServer.new(8881)
 
   http_server.add_content('GET', '/') do |request, response|
-    response.add_cookie('NAME=VALUE')
-    response.add_cookie('COUNT=1')
+    if request.header.has_key?('Cookie')
+      cookies = Hash[*request.header['Cookie'].split('; ').map {|kv| kv.split('=', 2)}.flatten]
+    else
+      cookies = {}
+    end
 
-    <<EOD
+    count = cookies.has_key?('count') ? cookies['count'].to_i : 0
+    count += 1
+
+    response.add_cookie("count=#{count}")
+
+    erb = ERB.new(<<EOD)
 <html>
   <body>
-    Hello, <font style="color:red">my server</font>!
+    <p>
+      Hello, <font style="color:red">my server</font>!
+    </p>
+    <p>
+      Count: <%= count %>
+    </p>
   </body>
 </html>
 EOD
+    erb.result(binding)
   end
 
   http_server.run

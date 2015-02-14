7245bffe0ffe6da4233471bfc45105705dcb2364
tyfkda <tyfkda@gmail.com>
2015-02-14 14:03:17 +0900

    Delete cookie using expires date

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require 'erb'
 require './my_http_server'
 
+def create_expiration_time
+  past = Time.now.getgm - 1
+  past.strftime('%a, %d %b %Y %H:%M:%S %Z')
+end
+
 def main
   http_server = MyHttpServer.new(8881)
 
@@ -25,6 +30,21 @@ def main
     <p>
       Count: <%= count %>
     </p>
+
+    <a href="delete-cookie.html">Delete cookie</a>
+  </body>
+</html>
+EOD
+    erb.result(binding)
+  end
+
+  http_server.add_content('GET', '/delete-cookie.html') do |request, response|
+    response.add_cookie("count=; expires=#{create_expiration_time}")
+    erb = ERB.new(<<EOD)
+<html>
+  <body>
+    Cookie Deleted.
+    <a href="/">Home</a>
   </body>
 </html>
 EOD
